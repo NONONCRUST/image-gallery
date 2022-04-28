1d99697f9ad8d0753271bdf9a0e993ead5d6d1de
nonon <nononcrust@gmail.com>
2022-04-28 23:45:34 +0900

    docs: "change README.md"

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import styled from "styled-components";
+import "./App.css";
+import ImageBox from "./components/ImageBox";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .text-center {
+    text-align: center;
+  }
+
+  .plus-box {
+    width: 100px;
+    height: 100px;
+    min-width: 100px;
+    min-height: 100px;
+    border: solid 1px #707070;
+    font-size: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    line-height: 24px;
+  }
+
+  .gallery-box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 80px;
+    cursor: pointer;
+  }
+
+  .row {
+    flex-direction: row;
+    gap: 100px;
+    overflow-x: scroll;
+    width: 100%;
+    justify-content: flex-start;
+    padding: 0 140px;
+  }
+
+  .image-box {
+    width: 200px;
+    height: 200px;
+    min-width: 200px;
+    min-height: 200px;
+  }
+
+  .image-box img {
+    width: 200px;
+    height: 200px;
+    min-width: 200px;
+    min-height: 200px;
+  }
+
+  input {
+    display: none;
+  }
+
+  @media screen and (max-width: 500px) {
+    .initial-box {
+      gap: 40px;
+    }
+
+    .row {
+      padding: 40px;
+    }
+  }
+`;
 
 function App() {
+  const [imageList, setImageList] = useState<string[]>([]);
+
+  // react-dropzone
+  const onDrop = useCallback(
+    (acceptedFiles: any) => {
+      if (acceptedFiles.length) {
+        acceptedFiles.forEach((file: any) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onloadend = (event) => {
+            setImageList([...imageList, event.target?.result as string]);
+          };
+        });
+      }
+    },
+    [imageList]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className={"gallery-box " + (imageList.length > 0 && "row")}>
+        {imageList.length === 0 && (
+          <div className="text-center">
+            이미지가 없습니다.
+            <br />
+            이미지를 추가해주세요.
+          </div>
+        )}
+        {imageList.map((image, idx) => (
+          <ImageBox key={image + idx} src={image} />
+        ))}
+        <div className="plus-box" {...getRootProps()}>
+          +
+        </div>
+        <input {...getInputProps()} />
+      </div>
+    </Container>
   );
 }
 
